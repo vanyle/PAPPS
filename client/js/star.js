39b697b40c279f8905e0d43abda60e3b4ce597e3
el-pasto <mathis@pastorelli.xyz>
2020-10-31 20:01:54 +0100

    Created recipe list item

---
+++ client/js/star.js
@@ -0,0 +1,43 @@
+class StarRating {
+  constructor(container, rating = 0) {
+    this.container = container;
+    this.setGraphics();
+    this.setRating(rating);
+  }
+
+  setRating(rating) {
+    this.rating = rating;
+    this.updateRating(this.rating);
+  }
+
+  updateRating(rate) {
+    this.container.querySelector(".full-star-container").style.clipPath = "inset(0 " + Math.round((1-rate)*100).toString() + "% 0 0)";
+  }
+
+  setGraphics() {
+    this.container.classList.add("star-rating");
+    this.container.innerHTML = '<div class="empty-star-container"> <i class="far fa-star"></i> <i class="far fa-star"></i> <i class="far fa-star"></i> <i class="far fa-star"></i> <i class="far fa-star"></i> </div> <div class="full-star-container"> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i></div>';
+  }
+
+  activateRating(ratingFunction) {
+    var self = this;
+    this.ratingFunction = ratingFunction;
+
+    this.container.addEventListener("mouseover", function(e) {
+      var rect = e.currentTarget.getBoundingClientRect();
+      var x = e.clientX - rect.left; //x position within the element.
+      var y = e.clientY - rect.top;  //y position within the element.
+      var xPerc = x/rect.width;
+      if(xPerc < 0.10) {
+        var rPerc = 0;
+      } else {
+        var rPerc = Math.trunc(5*xPerc + 1)/5;//rounded percentage to correspond to a full star
+      }
+      self.updateRating(rPerc);
+      self.ratingFunction(rPerc);
+    });
+    this.container.addEventListener("mouseleave", function(e) {
+      self.updateRating(self.rating);
+    });
+  }
+}
--- client/script.js
+++ client/script.js
@@ -43,6 +43,28 @@ if(displayed_section !== ""){
 	displaySection("home");
 }
 
+function titleToID(title) {
+	return title.replace("/ /g","-");
+}
+
+function displayRecipeCard(title, description, image, rating = 0) {
+	var id = titleToID(title);
+	var elemHTML = '<div class="list-card" id="' + id + '"> <img class="list-recipe-image" src="' + image +'" alt="Photo de la recette" width="150px"/> <div class="list-recipe-info-container"> <div class="list-recipe-info-top"> <h3 class="list-recipe-title">' + title + '</h3> <div class="sc"> </div> </div><p class="list-recipe-description">' + description + '</p> </div> </div>';
+	var list_cont = document.querySelector("#recipe-list-container");
+	var inn = list_cont.innerHTML;
+	inn += elemHTML;
+	list_cont.innerHTML = inn;
+
+	var elem = document.getElementById(id);
+	var starContainer = elem.querySelector(".sc");
+	var SR = new StarRating(starContainer, rating);
+	SR.setRating(rating);
+	return SR;
+
+}
+
+var sr = displayRecipeCard("Gratin de crozet aux poireaux","Une petite description de la recette : une recette tout droit venue de Savoie : miam miam le Beaufort, c tré bon ! Cela vous changera des raclettes et autres tartiflettes pour le Pistus...","media/paps.png",3.6/5);
+
 // Astuce pour avoir de la couleur dans la console.
 // console.log(trucaafficher);
 // marche tres bien dans 99% des cas.
@@ -50,5 +72,5 @@ console.log("%cHi :)","color: red;font-size:30px;");
 
 (async () => {
 	// Start the async stuff.
-	
+
 })();
